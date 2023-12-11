f34b6ea8280a94e9ac696d6e64496ce9baa3e1d8
unknown <ivanlorenzoruiz@alumnos.tuniversformacion.es>
2023-12-11 10:22:18 +0100

    Agregadas imagenes

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,7 @@
 
 window.onload= function () {
     const IMAGENES= [
-        'templates/840_560.jpg',
+        'assets/images/840_560.jpg',
         'assets/images/maxresdefault.jpg',
         'assets/images/mejor-pan-2.jpg',
         'assets/images/descarga.jpg'

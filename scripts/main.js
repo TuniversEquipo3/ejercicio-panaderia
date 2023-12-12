61e34497a5ee215cdd6883c36310c48aeb2adbd3
Torremolinos <adrian_2054@hotmail.com>
2023-12-12 13:34:38 +0100

    cambios para la ruta

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,17 @@
  * @github: https://github.com/TuniversEquipo3/ejercicio-panaderia
  */
 
+jQuery("#content1-headline1").fitText(1.0); // comprime el texto para que se ajuste al tamaño de la pantalla
+jQuery("#content1-headline2").fitText(1.0);
+jQuery("#content1-headline3").fitText(1.0);
+jQuery("#content2-1").fitText(2.0);
+jQuery("#content2-2").fitText(2.0);
+jQuery("#footer1").fitText(1.2);
+jQuery("#footer2").fitText(1.2);
+jQuery("#footer3").fitText(1.2);
+
+
+
 window.addEventListener("resize", function () {
   if (document.documentElement.clientWidth <= 430) {
     $(".navbar-brand").html("<img src='https://res.cloudinary.com/dbqqjaqqa/image/upload/v1489761620/logo_mini_pou3vz.png'>");
